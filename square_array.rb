95330e71d29c2e4966c2d3a132bb2d7e7a3139be
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-07-06 21:12:06 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-      new_array = []
-      array.each do |num|
-        squared_num = num ** 2
+  new_array = []
+  array.each do |num|
+  squared_num = num ** 2
         new_array.push(squared_num)
       end
       return new_array
